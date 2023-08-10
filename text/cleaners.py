98c53dd1b4770f4aff6fe5dbc2668734d6b9bb13
Nachanon Nuanphet <peumza1150@gmail.com>
2023-08-10 22:30:11 +0700

    WIP

--- text/cleaners.py
+++ text/cleaners.py
@@ -1,13 +1,8 @@
 import re
 from text.japanese import japanese_to_romaji_with_accent, japanese_to_ipa, japanese_to_ipa2, japanese_to_ipa3
 from text.korean import latin_to_hangul, number_to_hangul, divide_hangul, korean_to_lazy_ipa, korean_to_ipa
-from text.mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_romaji, chinese_to_lazy_ipa, chinese_to_ipa, chinese_to_ipa2
-from text.sanskrit import devanagari_to_ipa
 from text.english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
 from text.thai import num_to_thai, latin_to_thai
-from text.shanghainese import shanghainese_to_ipa
-from text.cantonese import cantonese_to_ipa
-from text.ngu_dialect import ngu_dialect_to_ipa
 
 
 def japanese_cleaners(text):
@@ -29,18 +24,7 @@ def korean_cleaners(text):
     return text
 
 
-def chinese_cleaners(text):
-    '''Pipeline for Chinese text'''
-    text = number_to_chinese(text)
-    text = chinese_to_bopomofo(text)
-    text = latin_to_bopomofo(text)
-    text = re.sub(r'([ˉˊˇˋ˙])$', r'\1。', text)
-    return text
-
-
 def zh_ja_mixture_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_romaji(x.group(1))+' ', text)
     text = re.sub(r'\[JA\](.*?)\[JA\]', lambda x: japanese_to_romaji_with_accent(
         x.group(1)).replace('ts', 'ʦ').replace('u', 'ɯ').replace('...', '…')+' ', text)
     text = re.sub(r'\s+$', '', text)
@@ -55,14 +39,10 @@ def sanskrit_cleaners(text):
 
 
 def cjks_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_lazy_ipa(x.group(1))+' ', text)
     text = re.sub(r'\[JA\](.*?)\[JA\]',
                   lambda x: japanese_to_ipa(x.group(1))+' ', text)
     text = re.sub(r'\[KO\](.*?)\[KO\]',
                   lambda x: korean_to_lazy_ipa(x.group(1))+' ', text)
-    text = re.sub(r'\[SA\](.*?)\[SA\]',
-                  lambda x: devanagari_to_ipa(x.group(1))+' ', text)
     text = re.sub(r'\[EN\](.*?)\[EN\]',
                   lambda x: english_to_lazy_ipa(x.group(1))+' ', text)
     text = re.sub(r'\s+$', '', text)
@@ -71,8 +51,6 @@ def cjks_cleaners(text):
 
 
 def cjke_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]', lambda x: chinese_to_lazy_ipa(x.group(1)).replace(
-        'ʧ', 'tʃ').replace('ʦ', 'ts').replace('ɥan', 'ɥæn')+' ', text)
     text = re.sub(r'\[JA\](.*?)\[JA\]', lambda x: japanese_to_ipa(x.group(1)).replace('ʧ', 'tʃ').replace(
         'ʦ', 'ts').replace('ɥan', 'ɥæn').replace('ʥ', 'dz')+' ', text)
     text = re.sub(r'\[KO\](.*?)\[KO\]',
@@ -85,8 +63,6 @@ def cjke_cleaners(text):
 
 
 def cjke_cleaners2(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_ipa(x.group(1))+' ', text)
     text = re.sub(r'\[JA\](.*?)\[JA\]',
                   lambda x: japanese_to_ipa2(x.group(1))+' ', text)
     text = re.sub(r'\[KO\](.*?)\[KO\]',
@@ -104,21 +80,9 @@ def thai_cleaners(text):
     return text
 
 
-def shanghainese_cleaners(text):
-    text = shanghainese_to_ipa(text)
-    text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
-    return text
-
-
 def chinese_dialect_cleaners(text):
-    text = re.sub(r'\[ZH\](.*?)\[ZH\]',
-                  lambda x: chinese_to_ipa2(x.group(1))+' ', text)
     text = re.sub(r'\[JA\](.*?)\[JA\]',
                   lambda x: japanese_to_ipa3(x.group(1)).replace('Q', 'ʔ')+' ', text)
-    text = re.sub(r'\[SH\](.*?)\[SH\]', lambda x: shanghainese_to_ipa(x.group(1)).replace('1', '˥˧').replace('5',
-                  '˧˧˦').replace('6', '˩˩˧').replace('7', '˥').replace('8', '˩˨').replace('ᴀ', 'ɐ').replace('ᴇ', 'e')+' ', text)
-    text = re.sub(r'\[GD\](.*?)\[GD\]',
-                  lambda x: cantonese_to_ipa(x.group(1))+' ', text)
     text = re.sub(r'\[EN\](.*?)\[EN\]',
                   lambda x: english_to_lazy_ipa2(x.group(1))+' ', text)
     text = re.sub(r'\[([A-Z]{2})\](.*?)\[\1\]', lambda x: ngu_dialect_to_ipa(x.group(2), x.group(
